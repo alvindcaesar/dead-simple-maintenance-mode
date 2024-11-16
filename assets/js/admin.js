9e02d6bcd095d3d12fb371ec793e4846196e750d
alvindcaesar <hello@alvindcaesar.com>
2024-11-17 00:50:28 +0800

    feat: Enhance settings page design

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,11 +1,7 @@
 jQuery(document).ready(function ($) {
     var pageField = $('.page-selector-row');
-
-    // Set initial state
     pageField.toggleClass('hidden-field', !$('#dsmm_activate').is(':checked'));
-
-    // Handle changes
-    $('#dsmm_activate').on('change', function () {
+    $('.dsmm-checkbox-wrapper input[type="checkbox"]').on('change', function () {
         pageField.toggleClass('hidden-field', !$(this).is(':checked'));
     });
 });
